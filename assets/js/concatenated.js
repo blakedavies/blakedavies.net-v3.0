c2bc9dbb1cb3974d757e47e6f5d0583d5809f3d4
Blake Davies <blakedavies1992@gmail.com>
2019-03-31 15:30:42 +1000

    Commit from GitHub Desktop

--- assets/js/concatenated.js
+++ assets/js/concatenated.js
@@ -3,7 +3,5 @@
 {% include_relative unconcatenated-js/can-touch.js %}
 {% include_relative unconcatenated-js/no-js.js %}
 {% include_relative unconcatenated-js/page-loader.js %}
-{% include_relative unconcatenated-js/pop-up.js %}
 {% include_relative unconcatenated-js/menu-icon.js %}
 {% include_relative unconcatenated-js/nav-slider.js %}
-{% include_relative unconcatenated-js/contact-form.js %}
