47825ee12f049a49a86a8921880ac30f03a89cd6
Blake Davies <blakedavies1992@gmail.com>
2019-04-19 19:28:34 +1000

    Update concatenated.js

--- assets/js/concatenated.js
+++ assets/js/concatenated.js
@@ -5,3 +5,4 @@
 {% include_relative unconcatenated-js/page-loader.js %}
 {% include_relative unconcatenated-js/menu-icon.js %}
 {% include_relative unconcatenated-js/nav-slider.js %}
+{% remote_include https://blakedavies.github.io/blakedavies-assets/js/nav-slider.js %}
